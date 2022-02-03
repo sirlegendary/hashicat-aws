c801029f6002a85333bf2a8463150d1d2e3e35f8
Wale <wale.salami@ecs.co.uk>
2022-02-03 18:25:21 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,7 +3,9 @@ module "s3_bucket" {
   version = "2.8.0"
 
   bucket = "my-s3-bucket"
-  bucket_prefix = var.prefix
+
+  bucket_prefix = "needstoberandom"
+  
   acl    = "private"
 
   versioning = {
