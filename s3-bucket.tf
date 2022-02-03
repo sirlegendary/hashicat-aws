5a46a3f35920064995ba3a9576959e4ae8b10cc8
Wale <wale.salami@ecs.co.uk>
2022-02-03 18:26:46 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,8 +2,6 @@ module "s3_bucket" {
   source  = "app.terraform.io/wale-play-ground/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
-
   bucket_prefix = "needstoberandom"
   
   acl    = "private"
